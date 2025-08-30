4eed5719e1d3f13dc5adcd5fb33e5e13674b390f
Ahmed Samy <as8609377@gmail.com>
2025-08-31 01:29:14 +0300

    feat(banner): add responsive banner section with dynamic background images and styled call-to-action button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,11 @@
-export default function Home() {}
+import Subnav from "@/layout/Subnav";
+import Banner from "@/modules/components/banner/Banner";
+
+export default function Home() {
+  return (
+    <>
+      <Subnav />
+      <Banner />
+    </>
+  );
+}
---
+++ src/modules/components/banner/Banner.tsx
@@ -0,0 +1,21 @@
+export default function Banner() {
+  return (
+    <div
+      className="w-full h-[632px] bg-cover bg-center flex flex-col   items-start justify-center  px-6 sm:px-12 md:px-30 gap-4
+        text-left bg-[url('/images/banner.webp')] sm:bg-[url('/images/banner.webp')]   md:bg-[url('/images/banner.webp')] max-sm:bg-[url('/images/Banner-mbulie.webp')] "
+    >
+      <div className="text-center md:text-start mb-30 ">
+        <p className="text-lg text-gray-400 font-semibold">Pro. Beyond.</p>
+        <h1 className="text-6xl md:text-7xl font-stretch-50% text-white ">iPhone 14 Pro</h1>
+        <p className="max-w-md mt-2 text-gray-400">Created to change everything for the better. For everyone.</p>
+        <button
+          className="mt-5 px-7 py-3 cursor-pointer border border-amber-50 text-white font-semibold rounded-lg  bg-transparent
+    hover:bg-indigo-800 hover:border-indigo-800
+    transition-colors duration-300 ease-in-out"
+        >
+          Shop Now
+        </button>
+      </div>
+    </div>
+  );
+}
