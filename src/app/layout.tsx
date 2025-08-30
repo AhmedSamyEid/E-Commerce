47a9675edb75fa32d18b68d9672e27b112e040a0
Ahmed Samy <as8609377@gmail.com>
2025-08-30 22:48:17 +0300

    feat(header): responsive header with mobile drawer, search and icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
-import "./globals.css";
+import "@/style/globals.css";
 import type { Metadata } from "next";
 import Header from "@/modules/Header";
+import Footer from "@/modules/Footer";
 import { Geist, Geist_Mono } from "next/font/google";
 
 const geistSans = Geist({
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/modules/Footer.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import { FaTwitter, FaFacebook, FaTiktok, FaInstagram } from "react-icons/fa";
+
+const services = ["Bonus program", " Gift cards", "Credit and payment", "Service contracts", "Non-cash account", "Payment"];
+const assistance = ["Find an order", "Terms of delivery", " Exchange and return of goods", "Guarantee", "Frequently asked questions", "Terms of use of the site"];
+
+export default function Footer() {
+  return (
+    <footer className="bg-black  items-center text-white py-10 px-6 sm:px-12">
+      <div className="flex  flex-col sm:flex-row justify-between gap-6">
+        <div>
+          <Image src="/images/Logo-white.webp" alt="logo Cyber" width={90} height={40} />
+          <p className="mt-5 leading-6">
+            We are a residential interior design firm located in Portland. Our
+            <br /> boutique-studio offers more than
+          </p>
+          <div className="flex mt-10 gap-7">
+            <FaTwitter />
+            <FaFacebook />
+            <FaTiktok />
+            <FaInstagram />
+          </div>
+        </div>
+
+        <div>
+          <h3 className="font-semibold">Services</h3>
+          {services.map((service) => (
+            <div key={service}>
+              <ul>
+                <li className="leading-10 text-[#cfcfcf] text-sm">{service}</li>
+              </ul>
+            </div>
+          ))}
+        </div>
+        <div>
+          <h3 className="font-semibold">Assistance</h3>
+          {assistance.map((assist) => (
+            <div key={assist}>
+              <ul>
+                <li className="leading-10 mb-1 text-[#cfcfcf] text-sm">{assist}</li>
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
+}
